00a8adb255a15ff3e3045b489284397e3609e421
Vasiliy <cartario@yandex.ru>
2020-06-22 08:55:15 +0300

    раздел dialogs

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,13 +3,18 @@ import './App.css';
 import Header from '../header/header.jsx';
 import Nav from '../nav/nav.jsx';
 import Profile from '../profile/profile.jsx';
+import Messages from '../messages/messages.jsx';
 
 function App() {
   return (
     <div className="app-wrapper">
 		<Header />
 		<Nav />
-		<Profile />
+		<div className="app-content-wrapper">
+			
+			<Messages />		
+		</div>
+		
     </div>
   );
 }
---
+++ src/components/messages/messages.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './messages.module.css';
+
+function Messages(){
+	return (		
+		<div className={s.container}>
+			<h1>Messages</h1>
+		</div>	
+	);
+}
+
+export default Messages;
