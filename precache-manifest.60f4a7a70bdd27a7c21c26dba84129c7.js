d53f6e433aed57ad94f6faab843f4d283bdb339b
VasiliyMac <cartario@yandex.ru>
2020-08-20 11:17:45 +0300

    Updates

---
+++ precache-manifest.60f4a7a70bdd27a7c21c26dba84129c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e340ae1c4041fb1f9414b5fbaf3dce03",
+    "url": "/it-kama-react/index.html"
+  },
+  {
+    "revision": "102da174e4030481b408",
+    "url": "/it-kama-react/static/css/main.b8b8f8b2.chunk.css"
+  },
+  {
+    "revision": "fcdb409a993199528ad3",
+    "url": "/it-kama-react/static/js/2.972cd276.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/it-kama-react/static/js/2.972cd276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "102da174e4030481b408",
+    "url": "/it-kama-react/static/js/main.f3fc1d5c.chunk.js"
+  },
+  {
+    "revision": "9bc98a48fe583820e3e6",
+    "url": "/it-kama-react/static/js/runtime-main.e7b8771b.js"
+  }
+]);
